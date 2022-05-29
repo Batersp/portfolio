09d027db9618cbeecb3c925137802ed1073a8a48
pavel_ishmukov <p-pup1@mail.ru>
2022-05-29 12:49:59 +0300

    add Header and Main

---
+++ src/main/Main.tsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import style from './Main.module.css'
+
+export const Main = () => {
+    return (
+        <div className={style.mainBlock}>
+            <div className={style.container}>
+                <div className={style.text}>
+                    <span>Hi There</span>
+                    <h1>I am Pavel Ishmukov</h1>
+                    <p>Frontend Developer </p>
+                </div>
+                <div className={style.photo}>
+
+                </div>
+            </div>
+        </div>
+    );
+};
+
